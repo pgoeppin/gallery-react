b020e14dcd8cf5e70f3c42bdd945c25dd5745d95
Pablo Goeppinger <pgoeppin@gmail.com>
2022-08-20 07:20:46 -0400

    Adding style and position to footer

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
 <>
 <Header titulo="CSGO SKINS"/>
 <div className='gallery'>
-  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_usp_silencer_cu_usp_printstream_light_large.9f5bcd4f94db3d965b3b2e7c3aace470e9466380.png' name='USP-S | Printstream' description='It has been custom painted with a stark black and white design and finished with pearlescent highlights.' alt=''/>
-  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_ak47_cu_ak47_asiimov_light_large.665fc3b83c8046398787a57d2ef7a9d938244820.png' name='AK-47 | Asiimov' description='It has been custom painted with a sci-fi design.' alt=''/>
-  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_awp_cu_medieval_dragon_awp_light_large.cb3b8168e59e96fd33efa9578206a2aaed036fc0.png' name='AWP | Dragon Lore' description='It has been custom painted with a knotwork dragon.' alt=''/>
-  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_knife_karambit_am_gamma_doppler_phase1_light_large.769cf2ab676ea2a7d7322c258f57bac8dca00336.png' name='Karambit | Gamma Doppler' description='It has been painted with black and silver metallic paints using a marbleizing medium, then candy coated.' alt=''/>
-  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_deagle_cu_deagle_kitch_light_large.0df9b5246d27786b413642f5fc959f37d8892c3a.png' name='Desert Eagle | Ocean Drive' description='It has been custom painted using oversaturated colors and different shapes.' alt=''/>
+  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_usp_silencer_cu_usp_printstream_light_large.9f5bcd4f94db3d965b3b2e7c3aace470e9466380.png' name='USP-S | Printstream' description='Pintado con un diseño en blanco y negro y con un acabado brillante.' alt='Una USP-S con un blanco perlado y con reflejos brillantes.'/>
+  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_ak47_cu_ak47_asiimov_light_large.665fc3b83c8046398787a57d2ef7a9d938244820.png' name='AK-47 | Asiimov' description='Está pintado con un diseño sacado de ciencia ficción.' alt='Un rifle AK-47 con colores blanco, negro y naranja'/>
+  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_awp_cu_medieval_dragon_awp_light_large.cb3b8168e59e96fd33efa9578206a2aaed036fc0.png' name='AWP | Dragon Lore' description='Está pintado con un detalle de un dragon celta.' alt='Un francotirador AWP dorado, pintada con un dragón.'/>
+  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_knife_karambit_am_gamma_doppler_phase1_light_large.769cf2ab676ea2a7d7322c258f57bac8dca00336.png' name='Karambit | Gamma Doppler' description='Está pintado con un negro y plateado metalizado.' alt='Un cuchillo tipo Karambit con un diseño negro y verdoso'/>
+  <Cards url='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_deagle_cu_deagle_kitch_light_large.0df9b5246d27786b413642f5fc959f37d8892c3a.png' name='Desert Eagle | Ocean Drive' description='Pintada con colores ultrasaturados y de diferentes formas.' alt='Una pistola Desert Eagle con colores saturados'/>
 </div>
 <Footer />
 </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import Button from 'react-bootstrap/Button';
 
 const Footer = () => {
     return(
-        <div className="text-center px-5">
+        <div className="footer text-center px-5 py-4">
             <h1>¿Que son las skins de CSGO?</h1>
-            <p>Son variantes de las texturas de las armas del videojuego Counter-Strike: Global Offensive, o más conocido como CSGO. Son armas con distintas texturas que se pueden equipar en el juego para mostrarse durante la partida. Son solo algo cosmético y tienen un valor de mercado.</p>
+            <p>Son variantes de las texturas de las armas del videojuego Counter-Strike: Global Offensive, o más conocido como CSGO. Son armas con distintas texturas que se pueden equipar en el juego para mostrarse durante la partida. Son solo algo cosmético y tienen un valor de mercado. Se pueden comprar en el mercado comunitario de la plataforma STEAM.</p>
             <Button>Ver más</Button>
         </div>
     );
